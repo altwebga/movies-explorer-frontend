010bc7015c5dc983b7c39c023d16629d683df7fd
altwebga <altwebga@gmail.com>
2022-11-13 12:15:49 +0700

    Add: auth, Login, Register, validation

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,31 +1,40 @@
-import React from "react";
-import "./Form.css";
-import Logo from "../../images/logo.svg";
+import React from 'react';
+import { NavLink } from "react-router-dom";
+import './Form.css';
+import Logo from '../../images/logo.svg';
 
-function Form({ title, children, btntext, text, path, pathname }) {
+function Form({ title, children, btntext, text, path, pathname, isValid, onSubmit }) {
+
   return (
-    <div className="form">
-      <div className="form-up">
-        <img className="logo" alt="Логотип" src={Logo} />
-        <h3 className="form-up-title">{title}</h3>
+    <div className='form'>
+
+      <div className='form-up'>
+        <NavLink to="/">
+          <img className='header__logo' src={Logo} alt='Лого' />
+        </NavLink>
+        <h3 className='form-up-title'>{title}</h3>
       </div>
 
-      <form className="form-content">{children}</form>
+      <form
+        className='form-content'
+        onSubmit={onSubmit}
+        noValidate
+      >
+        {children}
+        <div className='form-btn'>
+          <button className={`form-btn__item ${!isValid && "form-btn__item-disabled"
+            }`} disabled={isValid ? false : true} type='submit'>{btntext}</button>
 
-      <div className="form-btn">
-        <button className="form-btn__item" type="submit">
-          {btntext}
-        </button>
+          <p className='form-btn__item-text'>{text}
+            <a className='form-btn__item-path' href={path}>{pathname}</a>
+          </p>
 
-        <p className="form-btn__item-text">
-          {text}
-          <a className="form-btn__item-path" href={path}>
-            {pathname}
-          </a>
-        </p>
-      </div>
+        </div>
+      </form>
+
     </div>
-  );
+
+  )
 }
 
 export default Form;
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,27 +1,75 @@
-import React from "react";
-import "./Login.css";
-import Form from "../Form/Form";
+import React, { useState } from 'react';
+import './Login.css';
+import Form from '../Form/Form';
+import useFormWithValidation from '../../utils/validation.js';
 
-const Login = () => {
+function Login({ onLogin, message }) {
+  const [isMessage, setIsMessage] = useState(false)
+  const { values, handleChange, errors, isValid, resetForm } = useFormWithValidation();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onLogin({
+      email: values.email,
+      password: values.password
+    });
+    resetForm();
+    setIsMessage(true)
+  }
   return (
-    <Form
-      title="Рады видеть!"
-      btntext="Войти"
-      text="Ещё не зарегистрированы?"
-      path="/signup"
-      pathname="Регистрация"
+    <Form title='Рады видеть!'
+      btntext='Войти'
+      text='Ещё не зарегистрированы?'
+      path='/signup'
+      pathname='Регистрация'
+      isValid={isValid}
+      disabled={isValid}
+      onSubmit={handleSubmit}
     >
+
       <div className="input__content">
         <p className="input__text">E-mail</p>
-        <input className="input__element" required />
-        <span className="input__error" />
+        <input
+          className={`input__element ${!isValid && errors.name && 'input__error-activ'}`}
+          required
+          pattern="^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$"
+          autoComplete="on"
+          name='email'
+          text='email'
+          value={values.email || ''}
+          error={errors.email}
+          onInput={handleChange}
+        />
+        <span className="input__error">{errors.email}</span>
 
         <p className="input__text">Пароль</p>
-        <input className="input__element input__element-border-one" required />
-        <span className="input__error" />
+        <input
+          className={`input__element ${!isValid && errors.name && 'input__error-activ'}`}
+          required
+          name='password'
+          text='пароль'
+          type="password"
+          minLength="4"
+          maxLength="30"
+          value={values.password || ''}
+          error={errors.password}
+          onInput={handleChange}
+        />
+        <span className="input__error">{errors.password}</span>
       </div>
+
+      <p
+        className={
+          isMessage
+            ? 'input__error-log-res'
+            : ''
+        }
+      >
+        {message}
+      </p>
+
     </Form>
-  );
-};
+  )
+}
 
 export default Login;
